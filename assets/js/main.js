84f78739185eb0e81af1f59c5dcb90cf94d2b928
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-12 01:49:06 +0200

    Ex. complete
    add. functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,33 +21,35 @@ $(document).ready(function(){
   // create toto item
 
   btnSub.click(function(){
-    var newVal = inputText.val().trim();
-    $.ajax({
-      url : apiUrl,
-      method : 'POST',
-      data: {
-        text : newVal
-      }, success : function() {
-          printAll(apiUrl, todoList, template);
-      }, error : function() {
-        console.log('Errore nella lista');
-      }
-    });
+    // var newVal = inputText.val().trim();
+    // $.ajax({
+    //   url : apiUrl,
+    //   method : 'POST',
+    //   data: {
+    //     text : newVal
+    //   }, success : function() {
+    //       printAll(apiUrl, todoList, template);
+    //   }, error : function() {
+    //     console.log('Errore nella lista');
+    //   }
+    // });
+    createTodo ( apiUrl , inputText ,  template , todoList);
   });//click
 
   // delete this
   $(document).on('click', '.delete', function(){
-    var todoId = $(this).data('id');
-
-    $.ajax({
-      url : apiUrl + '/' + todoId,
-      method : 'DELETE',
-      success : function() {
-          printAll(apiUrl, todoList, template);
-      }, error : function() {
-        console.log('errore');
-      }
-    });
+    // var todoId = $(this).data('id');
+    //
+    // $.ajax({
+    //   url : apiUrl + '/' + todoId,
+    //   method : 'DELETE',
+    //   success : function() {
+    //       printAll(apiUrl, todoList, template);
+    //   }, error : function() {
+    //     console.log('errore');
+    //   }
+    // });
+    deleteTodo ( $(this), apiUrl, template, todoList)
   })//on
 
 });//ready
@@ -57,6 +59,39 @@ $(document).ready(function(){
 // ******* Functions ******
 // ************************
 
+// CRUD
+
+//  create new todo
+function createTodo ( apiUrl, input,  template, todoList) {
+  var newVal = input.val().trim();
+  $.ajax({
+    url : apiUrl,
+    method : 'POST',
+    data: {
+      text : newVal
+    }, success : function() {
+        printAll(apiUrl, todoList, template);
+    }, error : function() {
+      console.log('Errore nella lista');
+    }
+  });
+}; //createTodo
+
+
+// delete todo
+function deleteTodo ( self, apiUrl, template, todoList) {
+  var todoId = $(self).data('id');
+
+  $.ajax({
+    url : apiUrl + '/' + todoId,
+    method : 'DELETE',
+    success : function() {
+        printAll(apiUrl, todoList, template);
+    }, error : function() {
+      console.log('errore');
+    }
+  });
+}
 
 function printAll(apiUrl, todoList, template) {
   // reset
