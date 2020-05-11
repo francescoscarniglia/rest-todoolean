d4f6fdbe2edf1f4ef290e17b3941c9ce76c7103c
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-12 00:47:00 +0200

    continue work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,57 @@
 $(document).ready(function(){
 
   // Refs
-  var inputText = $('#textInput').val();
+  var inputText = $('#textInput');
   var btnSub = $('#submit');
   var todoList = $('.todoList');
-  var todo = $('.todo');
+
 
   // API
-  var apiUrl = '157.230.17.132:3020/todos';
+  var apiUrl = 'http://157.230.17.132:3020/todos';
 
   // Handlebars
-  var source = document.getElementById("todo-template").innerHTML;
+  var source = $('#todo-template').html();
   var template = Handlebars.compile(source);
+
+  // actions
+  // get todos
+
+  printAll(apiUrl, todoList, template);
+
 
 
 });//ready
+
+
+// ************************
+// ******* Functions ******
+// ************************
+
+
+function printAll(apiUrl, todoList, template) {
+  // reset
+  todoList.html('');
+
+  $.ajax({
+    url : apiUrl,
+    method : 'GET',
+    success : function(data){
+      for(i=0; i < data.length;i++) {
+        var todo = data[i];
+        console.log(todo);
+        var context = {
+          todo : todo.text,
+          id : todo.id
+        }
+
+        var html = template(context);
+
+        todoList.append(html);
+
+      } // for
+    }, error : function() {
+      console.log('Errore nella richiesta');
+    }
+  });
+
+};//printAll
