606a31ec971a4736ea4a7ff14e3cfa2cfdb66a6b
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-11 15:33:47 +0200

    Start js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  console.log('jQ ok');
-});//ready
 
-// 157.230.17.132:3020/todos
+  // Refs
+  var inputText = $('#textInput').val();
+  var btnSub = $('#submit');
+  var todoList = $('.todoList');
+  var todo = $('.todo');
+
+  // API
+  var apiUrl = '157.230.17.132:3020/todos';
+
+  // Handlebars
+  var source = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
+
+
+});//ready
